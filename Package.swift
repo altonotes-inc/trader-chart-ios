a8e4d7c69fe52b54bc164cef2118f45b6bff1f9e
山本敬太 <yamamoto@altonotes.co.jp>
2025-09-03 23:50:08 +0900

    First Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TraderChart",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "TraderChart",
+            targets: ["TraderChart"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "TraderChart"),
+        .testTarget(
+            name: "TraderChartTests",
+            dependencies: ["TraderChart"]
+        ),
+    ]
+)
